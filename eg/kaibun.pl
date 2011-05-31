46bc134b9de8d179b3cf8d48c776237a1da67e0d
Syohei YOSHIDA <syohex@gmail.com>
2011-05-31 23:04:28 +0900

    update anagram sample

--- eg/kaibun.pl
+++ eg/kaibun.pl
@@ -10,7 +10,13 @@ use App::GlitchIME;
 my $input = shift or die "Usage: $0 japanse_sentence";
 my $decoded = decode_utf8($input);
 
-my $text = $decoded . reverse(substr $decoded, 0, -1);
+my $text;
+if ((length $decoded) % 2 == 0) {
+    $text = $decoded . reverse $decoded;
+} else {
+    $text = $decoded . reverse(substr $decoded, 0, -1);
+}
+
 my $ime = App::GlitchIME->new(encode_utf8($text));
 
 for (1..10) {
